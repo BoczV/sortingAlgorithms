73445cbabb6d99ce3ef680e6c80733f26ba2df95
Viktor Bocz <beviktor95@gmail.com>
2020-09-27 20:04:47 +0200

    Implemented QuickSort

--- src/main/java/sortingtypes/QuickSort.java
+++ src/main/java/sortingtypes/QuickSort.java
@@ -1,38 +1,44 @@
+package sortingtypes;
+
 import java.util.Collections;
 import java.util.List;
 
 public class QuickSort {
 
     public void sort(List<Integer> toSort) {
-        Integer startingPoint = 0;
-        Integer endingPoint = toSort.size() - 1;
-        sort(toSort, startingPoint, endingPoint);
+        try {
+            Integer startingPoint = 0;
+            Integer endingPoint = toSort.size() - 1;
+            sort(toSort, startingPoint, endingPoint);
+        } catch (NullPointerException ex){
+            throw new IllegalArgumentException();
+        }
     }
 
-    private void sort(List<Integer> toSort, Integer startingPoint, Integer endingPoint) {
-        if (startingPoint >= endingPoint) {
-            return;
+        private void sort (List < Integer > toSort, Integer startingPoint, Integer endingPoint){
+            if (startingPoint >= endingPoint) {
+                return;
+            }
+            Integer pivot = toSort.get((startingPoint + endingPoint) / 2);
+            Integer index = partition(toSort, startingPoint, endingPoint, pivot);
+            sort(toSort, startingPoint, index - 1);
+            sort(toSort, index, endingPoint);
         }
-        Integer pivot = toSort.get((startingPoint + endingPoint) / 2);
-        Integer index = partition(toSort, startingPoint, endingPoint, pivot);
-        sort(toSort, startingPoint, index - 1);
-        sort(toSort, index, endingPoint);
-    }
 
-    private Integer partition(List<Integer> toSort, Integer startingPoint, Integer endingPoint, Integer pivot) {
-        while (startingPoint <= endingPoint){
-            while (toSort.get(startingPoint) < pivot){
-                startingPoint++;
+        private Integer partition (List < Integer > toSort, Integer startingPoint, Integer endingPoint, Integer pivot){
+            while (startingPoint <= endingPoint) {
+                while (toSort.get(startingPoint) < pivot) {
+                    startingPoint++;
+                }
+                while (toSort.get(endingPoint) > pivot) {
+                    endingPoint--;
+                }
+                if (startingPoint <= endingPoint) {
+                    Collections.swap(toSort, startingPoint, endingPoint);
+                    startingPoint++;
+                    endingPoint--;
+                }
             }
-            while (toSort.get(endingPoint) > pivot){
-                endingPoint--;
-            }
-            if (startingPoint <= endingPoint){
-                Collections.swap(toSort, startingPoint, endingPoint);
-                startingPoint++;
-                endingPoint--;
-            }
+            return startingPoint;
         }
-        return startingPoint;
     }
-}
--- src/test/java/QuickSortTest.java
+++ src/test/java/QuickSortTest.java
@@ -1,5 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import sortingtypes.QuickSort;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 class QuickSortTest {
+    private final QuickSort quickSort = new QuickSort();
+
+    @Test
+    void testSort(){
+        List<Integer> toSort = Arrays.asList(3, 1, 10, 8, 7, 9, 5, 2, 4);
+        quickSort.sort(toSort);
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 7, 8, 9, 10);
+        Assertions.assertIterableEquals(expected, toSort);
+    }
+
+    @Test
+    void testSortWithDuplicates(){
+        List<Integer> toSort = Arrays.asList(3, 1, 10, 8, 7, 9, 1, 5, 2, 4);
+        quickSort.sort(toSort);
+        List<Integer> expected = Arrays.asList(1, 1, 2, 3, 4, 5, 7, 8, 9, 10);
+        Assertions.assertIterableEquals(expected, toSort);
+    }
+
+    @Test
+    void testWithNull(){
+        List<Integer> toSort = null;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> quickSort.sort(toSort));
+    }
 
 }
