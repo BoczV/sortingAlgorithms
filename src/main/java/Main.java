d0bf50c94b247c2f4c41f520fdf6b0fbce66e493
Viktor Bocz <beviktor95@gmail.com>
2020-09-28 08:52:11 +0200

    Unused codesnippet deleted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        InsertionSort insertionSort = new InsertionSort();
-        List<Integer> toSort = Arrays.asList(3, 1, 10, 8, 7, 9, 5, 2, 4);
-        System.out.println(toSort);
-        insertionSort.sort(toSort);
-        System.out.println(toSort);
+        
     }
 }
